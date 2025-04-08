b21fd1c54513c56a1cea138b1a7ca5a1b46b2af9
= <=>
2025-04-08 19:06:38 +0200

    add watch utilities with wandb

--- deepwalk.py
+++ deepwalk.py
@@ -1,13 +1,20 @@
+import glob
+
 import torch
-from torch.utils.data import TensorDataset, 
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import yaml
 from networkx import generate_random_paths
 from torch import Tensor
+from torch.utils.data import DataLoader, TensorDataset
 from torch_geometric.utils import to_networkx
+from tqdm import tqdm
 
+import wandb
 from supervisedLinkPred import build_train_test_graphs
+
+wandb.init(project="challenge-graphs")
 
 train_graph, test_graph = build_train_test_graphs(
     "data/degrees_id_remapped.csv"
@@ -15,66 +22,108 @@ train_graph, test_graph = build_train_test_graphs(
 train_graph = to_networkx(train_graph, to_undirected=True)
 
 NUM_NODES = train_graph.number_of_nodes()
-EMBEDDING_DIM = 64
-PATH_LENGTH = 4  # that means 5 nodes in total
-NUM_NEGATIVE_SAMPLES = 2
+
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+
+EMBEDDING_DIM = config["embedding_dim"]
+LR = config["lr"]
+DATASET_SIZE = config["dataset_size"]
+PATH_LENGTH = config["path_length"]
+N_EPOCHS = config["n_epochs"]
+BATCH_SIZE = config["batch_size"]
 
 
 class DeepWalk(nn.Module):
-    def __init__(self):
+    def __init__(self, num_nodes: int, embedding_dim: int):
         super(DeepWalk, self).__init__()
-        self.contextEmbedding = nn.Embedding(
-            NUM_NODES,
-            EMBEDDING_DIM,
-        )
-        self.centerEmbedding = nn.Embedding(
-            NUM_NODES,
-            EMBEDDING_DIM,
-        )
+        self.contextEmbedding = nn.Embedding(num_nodes, embedding_dim)
+        self.centerEmbedding = nn.Embedding(num_nodes, embedding_dim)
 
-    def forward(self, x_center: int, x_context: list[int]):
+    def forward(self, x_center: int) -> Tensor:
         """
         x_center: int, the index of the node embedding as center node
-        x_context: Tensor[int], the indices of the node embeddings as context nodes. Includes postive and negative context
         """
         return (
-            self.contextEmbedding[x_context] @ self.centerEmbedding[x_center]
+            self.centerEmbedding(x_center) @ self.contextEmbedding.weight.T
         )  # similarity of the whole voc with x, the center word
 
 
-rw = generate_random_paths(train_graph, sample_size=3, path_length=PATH_LENGTH)
-rw_list = list(rw)
-print("rw list", rw_list)
-x_center = torch.tensor([path[len(path) // 2] for path in rw_list])
-# Take one node at random from the context
-rw_list_without_center = [
-    path[: len(path) // 2] + path[len(path) // 2 + 1 :] for path in rw_list
-]
-
-# print(rw_list_without_center)
-y = torch.tensor(
-    [
-        path[
-            torch.randint(low=0, high=len(rw_list_without_center[0]), size=(1,)).item()
-        ]
-        for path in rw_list_without_center
-    ]
+rw = generate_random_paths(
+    train_graph, sample_size=DATASET_SIZE, path_length=PATH_LENGTH
 )
-y_negative = torch.randint(NUM_NODES, (len(x_center), NUM_NEGATIVE_SAMPLES))
+rw_tensor = torch.tensor(list(rw))
+center_index = PATH_LENGTH // 2
+rw_context = torch.cat(
+    (rw_tensor[:, :center_index], rw_tensor[:, center_index + 1 :]), dim=1
+)
+rw_center = rw_tensor[:, center_index]
 
-x_context = torch.cat([y, y_negative], dim=0)
+y = torch.zeros((rw_tensor.size(0), NUM_NODES))
+for i in range(rw_tensor.size(0)):
+    context = rw_context[i]
+    y[i, context] = 1
 
 
-print("y_positive", y)
-print("y_neg", y_negative)
+train_dataset = TensorDataset(rw_center[:8], y[:8])
+test_dataset = TensorDataset(rw_center[8:], y[8:])
 
-dataset = 
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=False)
+test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-n_epochs = 2
-model = DeepWalk()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
-loss = nn.CrossEntropyLoss()
+print(next(iter(train_dataloader)))
 
-for i in range(n_epochs)
-    optimizer.zero_grad()
-    output = model(X)
+model = DeepWalk(NUM_NODES, EMBEDDING_DIM)
+optimizer = optim.Adam(model.parameters(), lr=LR)
+loss_fn = nn.BCEWithLogitsLoss()
+
+# profile_dir = "wandb/profiler"
+# profiler = torch.profiler.profile(
+#     schedule=schedule,  # see the profiler docs for details on scheduling
+#     on_trace_ready=torch.profiler.tensorboard_trace_handler(profile_dir),
+#     with_stack=True,
+# )
+
+# with profiler:
+#     ...  # run the code you want to profile here
+#     # see the profiler docs for detailed usage information
+
+
+wandb.watch(model, log="all", criterion=loss_fn, log_freq=10000, log_graph=True)
+
+for i in tqdm(range(N_EPOCHS)):
+    train_loss = 0.0
+    for batch_index, (X, y) in enumerate(train_dataloader):
+        optimizer.zero_grad()
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+        loss.backward()
+        optimizer.step()
+
+    with torch.no_grad():
+        test_loss = 0.0
+        for X, y in test_dataloader:
+            y_pred = model(X)
+            loss = loss_fn(y_pred, y)
+            test_loss += loss.item()
+
+    wandb.log(
+        {
+            "train_loss": train_loss / len(train_dataloader),
+            "test_loss": test_loss / len(test_dataloader),
+        }
+    )
+
+    if i % 5 == 0:
+        print(
+            f"Epoch {i + 1}/{N_EPOCHS}, Train Loss: {train_loss / len(train_dataloader)}"
+        )
+        print(f"Test Loss: {test_loss / len(test_dataloader)}")
+
+
+# # create a wandb Artifact
+# profile_art = wandb.Artifact("trace", type="profile")
+# # add the pt.trace.json files to the Artifact
+# profile_art.add_file(glob.glob(profile_dir + ".pt.trace.json"))
+# # log the artifact
