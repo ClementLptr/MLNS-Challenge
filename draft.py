23f5113f2c93d1c8cf7e30bcc78820c7947dad13
= <=>
2025-04-08 19:06:38 +0200

    add watch utilities with wandb

--- draft.py
+++ draft.py
@@ -5,4 +5,8 @@ import torch.optim as optim
 
 a = torch.tensor([[1, 2, 3, 4, 4, 5], [2, 3, 45, 4, 1, 3], [2, 3, 4, 1, 9, 4]])
 
-print(a[[0, 2]])
+ones_indices = torch.tensor([[0, 1], [1, 2], [2, 3]])
+
+a[:, ones_indices] = 1
+
+print(a)
